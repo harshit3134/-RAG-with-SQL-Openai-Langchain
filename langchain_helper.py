c99200e750da7441e698d4bbf00049e74bfbb7cd
Harshit Ajakiya <96171251+harshit3134@users.noreply.github.com>
2023-12-28 22:01:34 +0530

    Add files via upload

---
+++ langchain_helper.py
@@ -0,0 +1,22 @@
+import os
+from langchain.agents import *
+from langchain.llms import OpenAI
+from langchain.sql_database import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain.chat_models import ChatOpenAI
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+
+#conncetion to the database , change the password,user and db_name accordingly
+db = SQLDatabase.from_uri(f"mysql+pymysql://root:password@localhost/db_name")
+
+os.environ["OPENAI_API_KEY"] = ""
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+def get_answer(query):
+
+    db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
+    response=db_chain.run(query+"give me the response in a human manner")
+    return response
+
+get_answer("How many tables are there in the database?")
